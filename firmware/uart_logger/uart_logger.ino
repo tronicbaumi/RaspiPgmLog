1b992208387b34b3d473876036ee59feb9e2b15c
Constikri <constantin.krischke@stud.hshl.de>
2020-02-12 10:35:52 +0100

    updated logger firmware, to be fully functional: send data from 6 analogue ports and the status of 5 digital pwm outputs / 5 pwm output pins can be controlled over serial now

--- firmware/uart_logger/uart_logger.ino
+++ firmware/uart_logger/uart_logger.ino
@@ -3,13 +3,16 @@
  * Samples analog input at a given speed and sends it to the raspi
  */
 #include <stdlib.h>
+#include <string.h>
 
+// intitialise some variables
 unsigned long mstime;
 unsigned long counter;
 unsigned int interval;
-String str;
+char str[100]; // a char array used as string
 char c;
-int index;
+char* index; // a char pointer to store the output of the strstr function
+int DO[14] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // a int array to save the state of the digital pins of the Arduino
 
 bool sampling;
 
@@ -17,11 +20,18 @@ bool sampling;
 void setup() {
   //Setting the Baud to 230400 Bits/Second
   Serial.begin(230400);
+
+  // intitialise 5 pwm pins of the Arduino Uno
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  
   // initialise all variables with standard values
   mstime = millis();
   counter = 1;
-  str = "";
-  c = "";
+  sprintf(str, "");
   interval = 100;
   index = 0;
 
@@ -29,42 +39,83 @@ void setup() {
 }
 
 void loop() {
-  str = "";
+  // reset the string
+  sprintf(str, "");
   // read incomming data
   while(Serial.available()){
     c = Serial.read();
-    str = str + c;
-    index = str.indexOf("\n");
+    sprintf(str, "%s%c", str, c);
+    index = strstr (str,"\n");
     // process input if the whole line is read
-    if(index > -1){
-      str.remove(index);
+    if(index != NULL){
       Serial.println(str);
-      if(str == "start"){// start logging, reset counter
+      if(strcmp(str, "start\n") == 0){// start logging, reset counter
         sampling = true;
         counter = 0;
-      }else if(str == "stop"){ // stop logging
+      }else if(strcmp(str, "stop\n") == 0){ // stop logging
         sampling = false;
         Serial.println("stopped");
-      }else if(str.indexOf("v") > -1){ // set sample speed
+      }else if(strstr (str,"v")){ // set sample speed
+        char* pch;
+        unsigned int temp;
+        // parse the speed information string
+        pch = strtok (str,"|");
+        while (pch != NULL)
+        {
+          pch = strtok (NULL, "|");
+          if(pch != NULL){
+            temp = atoi(pch);  
+          }
+        }
         // check if input is valid, then proceede
-        unsigned int temp = str.substring(2).toInt();
         if(temp > 0){
           interval = temp;
           counter = 0;
         }else{
           Serial.println("speederror");
         }
+      }else if(strstr (str,"g")){ // set digital pins of the Arduino
+        int i = 0;
+        char* pch;
+        int temp[2];
+        // parse the pin information string
+        pch = strtok (str,"|");
+        while (pch != NULL)
+        {
+          pch = strtok (NULL, "|");
+          if(pch != NULL){
+            temp[i] = atoi(pch);
+          }
+          i++;
+        }
+        // check if input is valid, then proceede
+        if(i == 3){
+          DO[temp[0]] = temp[1]; 
+        }else{
+          Serial.println("pinerror");
+        }
       }else{
         Serial.println("argerror");
       }
-      str = "";
+      // reset the string
+      sprintf(str, "");
+    }
+  }
+
+  // set the pins of the Arduino to the values, that are saved in the array
+  for(int i = 0; i < 14; i++){
+    if(DO[i] > 0){
+      analogWrite(i, DO[i]);
+    }else{
+      digitalWrite(i, LOW);
     }
   }
 
   // send sample data if the interval is met
   if(((millis() - mstime) >= interval) && sampling){
     mstime = millis();
-    str = String(counter * interval) + "," + String(analogRead(A0)) + "," + String((analogRead(A1) + 100)) + "," + String((analogRead(A2) + 300));
+    sprintf(str, "%lu,%i,%i,%i,%i,%i,%i,%i,%i,%i,%i,%i", counter * interval, analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4), analogRead(A5), DO[5], DO[6], DO[9], DO[10], DO[11]);
+
     Serial.println(str);
     counter++;
   }
